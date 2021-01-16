065d80879e91b0378b3a89ce8ec31622f9f6b137
Youssef Hussein <youssef.hussein42@gmail.com>
2021-01-16 19:17:35 +0200

    beautify test.py

--- test.py
+++ test.py
@@ -22,9 +22,13 @@ gauss_elimination_pivoting = GaussianEliminationPivoting(matrix, result)
 gauss_seidel = GaussSeidel(matrix, result, [])
 
 print(f'Gauss Elim: {gaussian_elimination.solve()}')
+print(f'Gauss Elim Flags: div_by_zero: {gaussian_elimination.division_by_zero}, inf sol: {gaussian_elimination.infinite_solutions}, no sol: {gaussian_elimination.no_solution}')
 print(f'Gauss Jordan: {gauss_jordan.solve()}')
+print(f'Gauss Jordan Flags: div_by_zero: {gauss_jordan.division_by_zero}, inf sol: {gauss_jordan.infinite_solutions}, no sol: {gauss_jordan.no_solution}')
 print(f'LU Decomp: {lu_decomposition.solve()}')
+print(f'LU Decomp Flags: div_by_zero: {lu_decomposition.division_by_zero}, inf sol: {lu_decomposition.infinite_solutions}, no sol: {lu_decomposition.no_solution}')
 print(f'Gauss Elim Piv: {gauss_elimination_pivoting.solve()}')
+print(f'Gauss Elim Piv Flags: div_by_zero: {gauss_elimination_pivoting.division_by_zero}, inf sol: {gauss_elimination_pivoting.infinite_solutions}, no sol: {gauss_elimination_pivoting.no_solution}')
 print(f'Gauss Seidel: {gauss_seidel.solve()}')
 print(f'Gauss Sedal Flags: div_by_zero: {gauss_seidel.division_by_zero}, inf sol: {gauss_seidel.infinite_solutions}, no sol: {gauss_seidel.no_solution}')
 for i in gauss_seidel.iterations_list:
